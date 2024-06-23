fc6cca327e2061673fa3085ace74de01273580d3
sweetpotatoX <abdobahaa53@gmail.com>
2024-06-23 20:18:23 +0300

    "Added shebang line and docstrings to makeChange function"

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,4 +1,15 @@
+#!/usr/bin/python3
+
+"""Change making module.
+"""
+
+
 def makeChange(coins, total):
+
+    """Determines the fewest number of coins needed to meet a given
+    amount total when given a pile of coins of different values.
+    """
+    
     if total <= 0:
         return 0
     
