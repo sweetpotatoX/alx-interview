a4c63dde41c3bde53d52ba2bc26813f873e40389
sweetpotatoX <abdobahaa53@gmail.com>
2024-12-08 22:02:25 +0200

    dddd

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -4,8 +4,7 @@
 
 
 def rotate_2d_matrix(matrix):
-    """Rotates an m by n 2D matrix in place.
-    """
+    """Rotates an m by n 2D matrix in place."""
     if type(matrix) != list:
         return
     if len(matrix) <= 0:
