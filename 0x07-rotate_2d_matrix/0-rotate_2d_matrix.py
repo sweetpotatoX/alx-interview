cb77eea61f98fa6492f877727a4ef7d4f2d27f37
sweetpotatoX <abdobahaa53@gmail.com>
2024-12-08 22:03:17 +0200

    as

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -5,22 +5,22 @@
 
 def rotate_2d_matrix(matrix):
     """Rotates an m by n 2D matrix in place."""
-    if type(matrix) != list:
-        return
+
+
+if not isinstance(matrix,     if )        return
     if len(matrix) <= 0:
         return
-    if not all(map(lambda x: type(x) == list, matrix)):
-        return
-    rows = len(matrix)
-    cols = len(matrix[0])
+if not all(map(lambda x: isinstance(x,     if not all(map(lambda x: )        return
+    rows=len(matrix)
+    cols=len(matrix[0])
     if not all(map(lambda x: len(x) == cols, matrix)):
         return
-    c, r = 0, rows - 1
+    c, r=0, rows - 1
     for i in range(cols * rows):
         if i % rows == 0:
             matrix.append([])
         if r == -1:
-            r = rows - 1
+            r=rows - 1
             c += 1
         matrix[-1].append(matrix[r][c])
         if c == cols - 1 and r >= -1:
